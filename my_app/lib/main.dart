a4c07a3186181053795d358597908313af6c7e0a
Onkar Mallinath Potdar <141763167+OmkarOP-07@users.noreply.github.com>
2025-09-04 14:56:21 +0530

    playing with flutter properties

---
+++ my_app/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Namaste Dosto", style: TextStyle(fontSize: 10)),
+          backgroundColor: Colors.cyanAccent[700],
+          centerTitle: true,
+        ),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  decoration: const BoxDecoration(
+                    color: Colors.red,
+                    shape: BoxShape.rectangle,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.account_box_outlined),
+                          Text("OmkarOP"),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Center(
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  decoration: const BoxDecoration(
+                    color: Colors.yellow,
+                    shape: BoxShape.rectangle,
+                  ),
+                  child: Center(child: Text("Yellow")),
+                ),
+              ),
+              Center(
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  decoration: const BoxDecoration(
+                    color: Colors.green,
+                    shape: BoxShape.rectangle,
+                  ),
+                  child: Center(child: Text("Green")),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
